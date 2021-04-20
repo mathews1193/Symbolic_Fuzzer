4a68de4b40ec877509aa1d0fdc6b4264f3a0df05
mathews1193 <mathews1193@yahoo.com>
2021-04-20 16:57:20 -0500

    Added example for function1 and updated imports for main

--- Examples/function1.py
+++ Examples/function1.py
@@ -1,1 +1,11 @@
-def function1():
+def gcd(a: int, b: int) -> int:
+    if a < b:
+        c: int = a
+        a = b
+        b = c
+
+    while b != 0:
+        c: int = a
+        a = b
+        b = c % b
+    return a
--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 from fuzzingbook.ControlFlow import gen_cfg, to_graph
 from graphviz import Source
 import inspect
+from Examples import function1
 
-
-def hello(a, b):
-    if a < b:
-        c: int = a
-        a: int = b
-        b: int = c
-
-    while b != 0:
-        c: int = a
-        a: int = b
-        b: int = c % b
-    return a
-
-
-cfg = gen_cfg(inspect.getsource(hello))
+cfg = gen_cfg(inspect.getsource(function1.gcd))
 graph = to_graph(cfg)
 Source(graph)
