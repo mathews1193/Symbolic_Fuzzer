03b34f1cc067cf7623c78b0772d3860e73d72e4e
mathews1193 <mathews1193@yahoo.com>
2021-04-26 13:15:42 -0500

    updated the symbolic fuzzer to respond to some functions involving loops

--- main.py
+++ main.py
@@ -4,27 +4,55 @@ from graphviz import Source
 import inspect
 
 from Examples.examples import gcd, check_triangle, abs_value, quad_solver, calculate_sum
+from symbolic_fuzzer import SymbolicFuzzer
 
-with ArcCoverage() as cov:
-    gcd(64, 8)
-    abs_value(-30)
-    quad_solver(1, -5, -24)
-    calculate_sum(1, 2, 94, 6, 2, 8, 9, 20, 43, 2)  # 187
+print("1- GCD")
+print("2- Check Triangle")
+print("3- Absolute Value")
+print("4- Quad Solver")
+print("5- Sum")
+option = input("Enter Option")
+
+if option ==  "1":
+    symfz_ct = SymbolicFuzzer(gcd)
+elif option == "2":
+    symfz_ct = SymbolicFuzzer(check_triangle)
+elif option == "3":
+    symfz_ct = SymbolicFuzzer(abs_value)
+elif option == "4":
+    symfz_ct = SymbolicFuzzer(quad_solver)
+elif option == "5":
+    symfz_ct = SymbolicFuzzer(calculate_sum)
+else:
+    print("Invalid option! Try Again.")
+
+
+paths = symfz_ct.get_all_paths(symfz_ct.fnenter)
+
+print("Number of paths for function")
+print(len(paths))
+
+for i in range(len(paths)):
+    txt = "Path: {}"
+    print(txt.format(i))
+    constraints = symfz_ct.extract_constraints(paths[i].get_path_to_root())
+    print(constraints)
+
 
 # testing function 1
-Source(to_graph(gen_cfg(inspect.getsource(gcd)), arcs=cov.arcs()).render("CFG/GCD/GCD1"))
+
 
 # testing function 2
 
-Source(to_graph(gen_cfg(inspect.getsource(check_triangle)), arcs=cov.arcs()).render("CFG/Check_Triangle/Check_Triangle1"))
+#Source(to_graph(gen_cfg(inspect.getsource(check_triangle)), arcs=cov.arcs()).render("CFG/Check_Triangle/Check_Triangle1"))
 
 # testing function 3
 
-Source(to_graph(gen_cfg(inspect.getsource(abs_value)), arcs=cov.arcs()).render("CFG/Abs_value/Abs_value1"))
+#Source(to_graph(gen_cfg(inspect.getsource(abs_value)), arcs=cov.arcs()).render("CFG/Abs_value/Abs_value1"))
 
 # testing function 4
-Source(to_graph(gen_cfg(inspect.getsource(calculate_sum)), arcs=cov.arcs()).render("CFG/Calculate_sum/Calculate_sum1"))
+#Source(to_graph(gen_cfg(inspect.getsource(calculate_sum)), arcs=cov.arcs()).render("CFG/Calculate_sum/Calculate_sum1"))
 
 # testing function 5
-Source(to_graph(gen_cfg(inspect.getsource(quad_solver)), arcs=cov.arcs()).render("CFG/Quad Solver/Quad Solver1"))
+#Source(to_graph(gen_cfg(inspect.getsource(quad_solver)), arcs=cov.arcs()).render("CFG/Quad Solver/Quad Solver1"))
 
--- symbolic_fuzzer.py
+++ symbolic_fuzzer.py
@@ -1,56 +1,30 @@
 import ast
+import inspect
 
-from fuzzingbook.SymbolicFuzzer import SimpleSymbolicFuzzer, to_src
+from fuzzingbook.ControlFlow import PyCFG
+from fuzzingbook.SymbolicFuzzer import SimpleSymbolicFuzzer, to_src, PNode, to_single_assignment_predicates
 
 from Examples.examples import check_triangle, abs_value, calculate_sum, gcd, quad_solver
 
 
-class SimpleSymbolicFuzzer(SimpleSymbolicFuzzer):
-    def get_all_paths(self, fenter, depth=0):
-        if depth > self.max_depth:
-            raise Exception('Maximum depth exceeded')
-        if not fenter.children:
-            return [[(0, fenter)]]
-
-        fnpaths = []
-        for idx, child in enumerate(fenter.children):
-            child_paths = self.get_all_paths(child, depth + 1)
-            for path in child_paths:
-                # In a conditional branch, idx is 0 for IF, and 1 for Else
-                fnpaths.append([(idx, fenter)] + path)
-        return fnpaths
+class SymbolicFuzzer(SimpleSymbolicFuzzer):
+    def get_all_paths(self, fenter):
+        path_lst = [PNode(0, fenter)]
+        completed = []
+        for i in range(self.max_iter):
+            new_paths = [PNode(0, fenter)]
+            for path in path_lst:
+                # explore each path once
+                if path.cfgnode.children:
+                    np = path.explore()
+                    for p in np:
+                        if path.idx > self.max_depth:
+                            break
+                        new_paths.append(p)
+                else:
+                    completed.append(path)
+            path_lst = new_paths
+        return completed + path_lst
 
     def extract_constraints(self, path):
-        predicates = []
-        for (idx, elt) in path:
-            if isinstance(elt.ast_node, ast.AnnAssign):
-                if elt.ast_node.target.id in {'_if', '_while'}:
-                    s = to_src(elt.ast_node.annotation)
-                    predicates.append(("%s" if idx == 0 else "z3.Not%s") % s)
-                elif isinstance(elt.ast_node.annotation, ast.Call):
-                    assert elt.ast_node.annotation.func.id == self.fn_name
-                else:
-                    node = elt.ast_node
-                    t = ast.Compare(node.target, [ast.Eq()], [node.value])
-                    predicates.append(to_src(t))
-            elif isinstance(elt.ast_node, ast.Assign):
-                node = elt.ast_node
-                t = ast.Compare(node.targets[0], [ast.Eq()], [node.value])
-                predicates.append(to_src(t))
-            else:
-                pass
-        return predicates
-
-
-symfz_ct = SimpleSymbolicFuzzer(check_triangle)
-paths = symfz_ct.get_all_paths(symfz_ct.fnenter)
-constraints = symfz_ct.extract_constraints(paths[0])
-print("Number of paths for function")
-print(len(paths))
-
-print(len(constraints))
-
-for i in range(len(paths)):
-    txt = "Path: {}"
-    print(txt.format(i))
-    print(paths[i])
+        return [to_src(p) for p in to_single_assignment_predicates(path) if p]
